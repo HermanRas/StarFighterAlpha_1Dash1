a0f01a6fc12bda66ae755495463ad2387ecfe414
Herman Ras <hermanr@PetraGroup.local>
2019-04-24 18:43:57 +0200

    Fixed max width & hight

--- JS/jets.js
+++ JS/jets.js
@@ -11,19 +11,20 @@ class jet_lv1 {
     move(x, y) {
         this.location.x += x * this.speed;
         this.location.y += y * this.speed;
-
-        // if (this.location.x < 0) {
-        //     this.location.x = 0;
-        // }
-        // if (this.location.x > screenHeight - this.size.x) {
-        //     this.location.x = screenHeight - this.size.x;
-        // }
-        // if (this.location.y + screenWidth + this.size.y > screenWidth) {
-        //     this.location.y = 0 - this.size.y;
-        // }
-        // if (this.location.y + screenWidth < 0) {
-        //     this.location.y = 0 - (screenWidth);
-        // }
+        //FIX MAX WIDTH
+        if (this.location.x < 0) {
+            this.location.x = 0;
+        }
+        if (this.location.x > screenWidth - this.size.x) {
+            this.location.x = screenWidth - this.size.x;
+        }
+        //FIX MAX HIGHT
+        if (this.location.y < 0) {
+            this.location.y = 0;
+        }
+        if (this.location.y > screenHeight - this.size.y) {
+            this.location.y = screenHeight - this.size.y;
+        }
     }
 
     show() {
@@ -32,6 +33,11 @@ class jet_lv1 {
         //imageMode(CORNER);
         //          NAME                  SW,              SH,          IW,         IH
         image(this.imgModel, this.location.x, this.location.y, this.size.x, this.size.y);
+    }
+
+    shoot() {
+        //                   SW,  SH, IW,IH
+        image(this.imgModel, 100, 300, 30, 8);
     }
 
 }
